74e569b89a6feb56338f4140083a3c44ca7fbf0e
Patrice Peterson <runiq@archlinux.us>
2013-12-12 14:25:44 +0100

    Back to bash, back to reality
    Fish as a login shell was a bad idea because it lacks POSIX compliance. I'm
    switching back to bash as a login shell, but execute fish for all login shells
    (in ~/.bashrc).

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,14 +1,8 @@
 #! /usr/bin/fish
 # vim: set foldmethod=marker filetype=sh:
 # Some things should only be done for login terminals
-if status --is-login
-  set -x LANG de_DE.UTF-8
-  source $HOME/.profile
-  eval "exec xinit $HOME/.xinitrc"
-end
-
 # Some things should be done for all interactive shells
 if status --is-interactive
   # Set up aliases
-  source $HOME/.config/fish/aliases.fish
+  source "$HOME/.config/fish/aliases.fish"
 end
