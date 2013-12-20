75aeea926eba7684f45f9f48f61c204550e73323
Patrice Peterson <runiq@archlinux.us>
2013-12-20 19:34:37 +0100

    Fish: Better aliases

--- files/config/fish/aliases.fish
+++ files/config/fish/aliases.fish
@@ -2,13 +2,15 @@
 # vim: set foldmethod=marker:
 
 # Slashes are no fun on a German keyboard
+alias .. "cd .."
 alias ... "cd ../.."
 alias .... "cd ../../.."
 alias ..... "cd ../../../.."
 
 alias ls "ls -F -h --color --group-directories-first -X"
-alias la "ls -a"
+alias la "ls -A"
 alias ll "ls -l"
+alias lla "ls -la"
 
 # I'm a lazy bastard
 alias p "pacaur"
