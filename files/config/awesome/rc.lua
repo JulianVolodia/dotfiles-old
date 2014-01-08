b4fc60d4695bde2f893af6b2d40410690af13efe
Patrice Peterson <runiq@archlinux.us>
2014-01-08 14:58:17 +0100

    Awesome: Remove unneeded comments

--- files/config/awesome/rc.lua
+++ files/config/awesome/rc.lua
@@ -43,32 +43,20 @@ theme = "monokai.lua"
 themedir = os.getenv("XDG_DATA_HOME") .. "/awesome/themes/"
 beautiful.init(themedir .. theme)
 
--- This is used later as the default terminal and editor to run.
+-- This is used later as the default terminal and editor to run
 terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = editor
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
+-- Default modkey
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
     awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
     awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier
 } -- }}}
 
 -- Wallpaper {{{
