135ee118e7316d7ec91b296ea8c6b3918e95dc6e
Patrice Peterson <runiq@archlinux.us>
2014-01-19 22:09:19 +0100

    Fish: Complete aliases
    This is preliminary as long as fish doesn't support this by default.

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -2,6 +2,13 @@
 
 # Some things should be done for all interactive shells
 if status --is-interactive
+  # Allows to complete aliases as if they were the proper commands 
+  function alias_with_completion --argument alias command
+    complete -c $alias -xa "(
+    set -l cmd (commandline -pc | sed -e 's/^ *\S\+ *//' );
+    complete -C\"$command \$cmd\";
+    )"
+  end
   # Set up aliases
   source "$HOME/.config/fish/aliases.fish"
 end
